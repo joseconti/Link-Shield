9f09e490c24001ea3a246f58f105cfdf8edc44d4
José Conti <j.conti@joseconti.com>
2024-10-14 13:03:36 +0200

    Code is poetry

--- uninstall.php
+++ uninstall.php
@@ -1,17 +1,21 @@
 <?php
 /**
  * Uninstall Link Shield
+ *
  * @version     0.5.0
+ * @package     Link Shield
  */
- if( !defined('WP_UNINSTALL_PLUGIN') ) exit();
 
- //Remove Options used by Link Shield
- delete_site_option( 'link_shield_text'								);
- delete_site_option( 'link_shield_shordcode'						);
- delete_site_option( 'link_shield_blog_show_link_text'				);
- delete_site_option( 'link_shield_blog_comments_show_link_text'		);
- delete_site_option( 'link_shield_bbpress_show_link_text'			);
- delete_site_option( 'link_shield_add_nofollow_to_comments_links'	);
- delete_site_option( 'link_shield_buddypress_show_link_text'		);
- delete_site_option( 'link_shield_hidden_text_message'				);
- ?>
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+// Remove Options used by Link Shield.
+delete_site_option( 'link_shield_text' );
+delete_site_option( 'link_shield_shordcode' );
+delete_site_option( 'link_shield_blog_show_link_text' );
+delete_site_option( 'link_shield_blog_comments_show_link_text' );
+delete_site_option( 'link_shield_bbpress_show_link_text' );
+delete_site_option( 'link_shield_add_nofollow_to_comments_links' );
+delete_site_option( 'link_shield_buddypress_show_link_text' );
+delete_site_option( 'link_shield_hidden_text_message' );
